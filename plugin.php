aa3acd6a3cdc3cbc230cafa8b0025a2e69967c12
Freeman <nabil@instafork.com>
2017-04-12 19:43:31 +0100

    Create plugin.php

---
+++ plugin.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Plugin Name: LeSalon Wordpress 
+ * Description: For customizing Wordpress 
+ * Author: nabilfreeman<nabil+oss@freemans.website>
+ * Author URI: http://freemans.website
+ * Version: 1.0.0
+ * Plugin URI: https://github.com/lesalonapp/lesalon-wordpress
+ */
+ 
+add_action('admin_head', 'lesalon_admin_logo');
+
+function lesalon_admin_logo() {
+  echo '<style>
+    li#wlcms_admin_logo{
+      display:-webkit-flex;
+      display:flex;
+      -webkit-align-items:center;
+      align-items:center;
+      height:32px;
+    }
+    
+    li#wlcms_admin_logo span{
+      float:none !important;
+      width:auto !important;
+      height:auto !important;
+      line-height:normal !important;
+      margin:0px 10px !important;
+    }
+    
+    li#wlcms_admin_logo span img{
+      width:auto !important;
+    }
+  </style>';
+}
+
+?>
